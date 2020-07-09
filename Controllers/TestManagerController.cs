79813448a14781b0211be7a9561a588082e39320
Rex-xx-glitch <67285672+Rex-xx-glitch@users.noreply.github.com>
2020-07-09 14:13:56 +0300

    Initial addition of files via upload

---
+++ Controllers/TestManagerController.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+
+namespace Quizify.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TestManagerController : ControllerBase
+    {
+        
+    }
+}
